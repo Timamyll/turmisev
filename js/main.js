0f71a0d8a611ccfaf599fc4bbd33279abec887ce
Timamyll <104847527+Timamyll@users.noreply.github.com>
2023-01-08 11:37:33 +0600

    final

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(".slider__body").slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+  });
+});
+
+$(document).ready(function () {
+  $(".card__box-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+  });
+});
